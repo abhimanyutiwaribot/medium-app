3e1d66ae12813cb36a38d6b31cb4d4b956697cef
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-05 21:30:20 +0530

    add seperate prisma client for both prod and dev

---
+++ backend/src/lib/prisma.ts
@@ -0,0 +1,35 @@
+import "dotenv/config.js"
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+// import { withAccelerate } from "@prisma/extension-accelerate";
+import { Pool } from "pg";
+
+// const connectionString = `${process.env.DATABASE_URL}`
+export function getPrismaClient(database_url: string) {
+    if(!database_url){
+        throw new Error("database_url is not defined")
+    }
+    const pool = new Pool({
+        connectionString: database_url
+    })
+    const adapter = new PrismaPg(pool)
+
+    const prisma = new PrismaClient({
+        adapter
+    })
+
+    return prisma
+}
+
+
+/*
+For production using prisma accelerate.
+
+export function getPrismaClient(accelerate_url: string){
+    const prisma = new PrismaClient({
+        accelerateUrl: accelerate_url
+    }).$extends(withAccelerate())
+
+    return prisma
+}
+*/
