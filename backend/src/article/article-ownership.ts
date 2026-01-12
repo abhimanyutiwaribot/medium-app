e6cdeed5072a3a62339731fb475db3464bd4d93f
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-12 19:15:29 +0530

    added article ownership service

---
+++ backend/src/article/article-ownership.ts
@@ -0,0 +1,31 @@
+import { PrismaDB } from "../types/prisma";
+
+export async function articleOwnership(
+  prisma: PrismaDB,
+  articleId: string,
+  userId: string
+){  
+  const article = await prisma.article.findUnique({
+    where:{
+      id: articleId
+    },
+    select:{
+      id: true,
+      authorId: true,
+      published: true,
+      published_At: true,
+      current_version: true
+    }
+  })
+
+  if(!article){
+    throw new Error("Article not found");
+  }
+
+  if(article.authorId !== userId){
+    throw new Error("Unauthorized");
+  }
+
+  return article;
+
+}
