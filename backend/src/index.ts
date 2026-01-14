bc5d1febda2ded82e6e5ebf8ea7bd2432401de06
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-14 18:19:33 +0530

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,30 @@
 import { Hono } from 'hono'
 import user from './routes/user-routes'
 import article from './routes/article-routes'
+import { cors } from 'hono/cors'
+import { resolve } from 'node:dns'
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use("*", cors({
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+  ],
+  allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
+app.get('/api/v1/health', (c) => {
+  try{
+    return c.json({
+    message: "i am fine !!"
+  }, 201)
+  }catch(e){
+    return c.json({
+      error: "i am dead"
+    }, 501)
+  }
 })
 
 
