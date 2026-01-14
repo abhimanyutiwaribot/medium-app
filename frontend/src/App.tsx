bb25bda8a270e94c2692c9f15027ce7ad97d6705
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-14 22:54:39 +0530

    add app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+// import { apifetch } from './api/client'
+import './App.css'
+import Signup from './pages/sign-up'
+import Signin from './pages/sign-in'
+import Editor from './pages/editor'
+
+function App() {
+
+  // async function testAPI(){
+  //   try{
+  //     const res = await apifetch("/health");
+  //     console.log(res);
+
+  //   }catch(e){
+  //     console.error(e)
+  //   }
+  // }
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element= {<Signup/>}/>
+          <Route path="/signin" element= {<Signin/>}/>
+          <Route path="*" element= {<Navigate to={"/signin"}/>}/>
+          <Route path="/editor" element={<Editor/>} />
+          <Route path="/profile" />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <button onClick={testAPI}>Test API</button> */}
+    </div>
+  )
+}
+
+export default App
