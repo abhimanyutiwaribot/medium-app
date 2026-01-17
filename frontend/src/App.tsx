a34c8f4f8147aefb777ff6aa071fc054842f66fc
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-17 21:41:28 +0530

    add protected routes to the necessary route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,34 +5,100 @@ import Signup from './pages/sign-up'
 import Signin from './pages/sign-in'
 import Editor from './pages/editor'
 import Article from './pages/article'
+import type { JSX } from 'react'
+import VersionHistoryPage from './pages/version-history'
+import ArticleDiffPage from './pages/diff-history'
+import HomePage from './pages/home'
+import ArticleVersion from './pages/article-version'
+import Layout from './components/layout'
+import DraftsPage from './pages/draft-articles'
+// import ArticleDiffPage from './components/diff/diff-function'
+
+function ProtectedRoute({ children }: { children: JSX.Element }) {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children
+}
 
 function App() {
-
-  // async function testAPI(){
-  //   try{
-  //     const res = await apifetch("/health");
-  //     console.log(res);
-
-  //   }catch(e){
-  //     console.error(e)
-  //   }
-  // }
-
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element= {<Signup/>}/>
-          <Route path="/signin" element= {<Signin/>}/>
-          <Route path="*" element= {<Navigate to={"/signin"}/>}/>
-          <Route path="/editor" element={<Editor/>} />
-          <Route path="/article/:id" element={<Article />} />
+          <Route path="/signup" element={<Signup />} /> {/* the signup page */}
+          <Route path="/signin" element={<Signin />} />  {/* the signin page */}
 
-          <Route path="/profile" />
+          <Route path="/article/:id" 
+            element={
+            <Layout>
+              <Article />
+            </Layout>
+            } /> {/* the article renderer page based on articleId */}
+
+          {/* <Route 
+            path="/article/:id"
+            element={
+              <ProtectedRoute>
+                <ArticleVersion />
+              </ProtectedRoute>
+            }
+          />   */}
+
+          <Route
+            path='/article/drafts'
+            element= {
+              <ProtectedRoute>
+                <DraftsPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="/editor" element={
+            <ProtectedRoute>
+              <Editor />
+            </ProtectedRoute>
+          }
+          />  {/* the editor page */}
+
+          <Route path="/editor/:id" element={
+            <ProtectedRoute>
+              <Editor />
+            </ProtectedRoute>
+          }
+          />  {/* another editor page for editing */}
+
+
+          <Route path="/article/:id/vS" element={
+            <ProtectedRoute>
+              <VersionHistoryPage />
+            </ProtectedRoute>
+          }
+          />  {/* the editor page */}
+
+          <Route
+            path="/article/:id/diff"
+            element={
+              <ProtectedRoute>
+                <ArticleDiffPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/"
+            element= {
+              <Layout>
+                <HomePage />
+              </Layout>
+            }
+          />
+
+          <Route path="*" element={<Navigate to="/signin" replace />} />  {/* fallback */}
         </Routes>
       </BrowserRouter>
-
-      {/* <button onClick={testAPI}>Test API</button> */}
     </div>
   )
 }
