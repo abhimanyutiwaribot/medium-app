a371fa7debc8a0267d62a685f2cfc10a4edace17
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-17 21:57:56 +0530

    new: get article diff

---
+++ backend/src/article/article-diff.ts
@@ -0,0 +1,35 @@
+import { PrismaDB } from "../types/prisma";
+import { articleOwnership } from "./article-ownership";
+
+export async function getArticleDiff(
+  prisma: PrismaDB,
+  articleId: string,
+  userId: string,
+  from: number,
+  to: number
+){
+
+  const article = await articleOwnership(
+      prisma,
+      articleId,
+      userId
+    )
+  
+    const versions = await prisma.articleVersion.findMany({
+      where: {
+        articleId,
+        version: { in: [from, to] },
+      },
+    });
+  
+    if(versions.length !== 2){
+      throw new Error("Version not found") 
+    }
+  
+    return {
+      from,
+      to,
+      fromContent: versions.find(v => v.version === from)?.content,
+      toContent: versions.find(v => v.version === to)?.content,
+    }
+} 
