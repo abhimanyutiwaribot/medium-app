2ebe585852484f37ae1a446571705611846a99cb
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-22 21:10:01 +0530

    ui revaming started

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -12,15 +12,20 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="border-b">
-      <div className="max-w-5xl mx-auto px-6 py-4 flex justify-between items-center">
+    <nav className="border-none">
+      <div className="max-w-full mx-auto px-6 py-4 flex justify-between items-center font-serif font-normal">
+        <div>  {/* todo implement sidebar */}
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </div>
         {/* Left */}
-        <Link to="/" className="text-xl font-semibold">
+        <Link to="/" className="text-2xl ">
           Xedium
         </Link>
 
         {/* Right */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-6">
           {!isAuthenticated && (
             <>
               <Link to="/signin" className="text-sm">
@@ -39,16 +44,19 @@ export default function Navbar() {
             <>
               <Link
                 to="/editor"
-                className="text-sm font-medium"
+                className="text-sm border rounded-md p-2 bg-emerald-900 text-[#b9c0c9] border-transparent"
               >
-                Write
+                + Write
               </Link>
 
               <button
-                onClick={logout}
-                className="text-sm text-red-600"
+                onClick={logout}     // have to change it to profile picture
+                
               >
-                Logout
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+
               </button>
             </>
           )}
