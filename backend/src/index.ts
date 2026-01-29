1f0348fed2076f75f9aaa6660cdc60d229f72c03
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-29 23:19:14 +0530

    added run event worker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,16 @@ import { cors } from 'hono/cors'
 // import { resolve } from 'node:dns'
 import image from './routes/image-route'
 import feed from './routes/feed-routes'
-const app = new Hono()
+import profile from './routes/profile-routes'
+import { runEventWorker } from './workers/event-worker'
+
+interface Env {
+  ACCELERATE_URL: string
+}
+
+const app = new Hono<{
+  Bindings: Env
+}>
 
 app.use("*", cors({
   origin: [
@@ -35,6 +44,20 @@ app.route('/api/v1/user', user);
 app.route('/api/v1/q', article);
 app.route('/api/v1/image', image);
 app.route('/api/v1/a', feed)
+app.route('/api/v1/profile', profile)
 
 
-export default app
+export default {
+   fetch: app.fetch,
+   async scheduled(
+    event: ScheduledEvent,
+    env: Env,
+    ctx: ExecutionContext
+   ){
+    console.log('Scheduled event triggered at:', new Date(event.scheduledTime))
+    console.log('ACCELERATE_URL:', env.ACCELERATE_URL ? 'exists' : 'missing')
+    ctx.waitUntil(
+      runEventWorker(env.ACCELERATE_URL)
+    )
+   }
+}
