9b20b284f004a0c7a1168c95cbbc57cfe7cb587c
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-29 23:13:50 +0530

    change in name

--- frontend/src/pages/sign-up.tsx
+++ frontend/src/pages/sign-up.tsx
@@ -4,7 +4,7 @@ import WelcomeText from "../components/welcome/welcome-text";
 export default function Signup() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center ">
-      <WelcomeText text ={"Join Medium."}/>
+      <WelcomeText text ={"Join Xedium."}/>
       <br />
       <SignupForm/>
       <br />
