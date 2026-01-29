19801667e914d6805916fa1043030cbc6a52e0ab
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-01-29 23:12:02 +0530

    no clue

--- frontend/src/components/welcome/welcome-text.tsx
+++ frontend/src/components/welcome/welcome-text.tsx
@@ -4,7 +4,7 @@ type Text = {
 
 export default function WelcomeText({text}: Text){
   return(
-    <div className="text-slate-900 font-comic text-2xl font-thin">
+    <div className=" font-comic text-2xl font-thin">
       {text}
     </div>
   )
