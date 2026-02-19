c2e1b15187d098705ec88f3989d9f1a439f680ec
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-02-19 19:29:49 +0530

    changes in theme -- have to look  after this for better themes

--- frontend/src/themes/index.ts
+++ frontend/src/themes/index.ts
@@ -2,7 +2,6 @@ export type ThemeKey =
   | "grainy-paper"
   | "grainy-dark"
   | "dark-stone"
-  | "black-gray"
   | "blackgray-two"
   | "blackgray-normal"
   | "dark"
@@ -13,6 +12,7 @@ export type ThemeKey =
   | "orange-black"
   | "red"
   | "blue"
+  | "light-blue"
 
 export const THEMES: Record<
   ThemeKey,
@@ -20,89 +20,91 @@ export const THEMES: Record<
     name: string;
     url: string;
     overlay: "light" | "dark";
+    color?: string;
   }
 > = {
   "grainy-paper": {
     name: "Grainy Paper",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768726067/theme-grainy-paper_wany8s.jpg",
-    overlay: "dark",
-    
+    overlay: "light",
+    color: "#601111ef" 
   },
   "grainy-dark": {
     name: "Grainy Paper Dark",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768726068/theme-grainy-paper-with-some-dark-texture_t3zf1h.jpg",
     overlay: "dark",
-    
+    color: "#080101ff" 
   },
   "dark-stone": {
     name: "Grainy Darkstone Desk",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768726063/theme-grainy-dark-stone-desk-texture_hjx9aw.jpg",
     overlay: "dark",
-    
+    color: "#e2fff5ff" 
   },
-  "black-gray": {
-    name: "Grainy Black Gray",
-    url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768725329/theme-grainy-black-gray_ezdk4x.jpg",
+  "light-blue": {
+    name: "Grainy Light Blue",
+    url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1771428682/Light_Colored_Backgrounds_t6sba3.jpg",
     overlay: "dark",
-    
+    color: "#ffffffed"
   },
   "blackgray-two": {
     name: "Grainy Black Gray 2",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768725328/theme-grainy-blackgray-2_zyoos4.jpg",
     overlay: "dark",
-    
+    color: "#ffffffff"
   },
   "blackgray-normal": {
     name: "Grainy Black Gray Normal",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768725309/theme-grainy-blackgray-normal_gwfhkr.jpg",
     overlay: "dark",
-    
+    color: "#ffffffff"
   },
   "dark": {
     name: "Grainy Dark",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768725288/theme-grainy-dark_iesuhk.jpg",
     overlay: "dark",
-    
+    color: "#c0acacff"
   },
   "dark-lavander": {
     name: "Grainy Dark Lavander",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768725273/theme-grainy-dark-lavander_p6fc5a.jpg",
     overlay: "dark",
-    
+    color: "#db6eecff"
   },
   "dark-ocean": {
     name: "Grainy Dark Ocean",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768725260/theme-grainy-dark-ocean_zqummn.jpg",
     overlay: "dark",
-    
-
+    color: "#cdfaf7ff"
   },
   "film-dust-scratches": {
     name: "Grainy Film Dust Scratches",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768725212/theme-grainy-film-dust-scratches_ey4ql1.jpg",
     overlay: "dark",
-    
+    color: "#ffffffff"
   },
   "ocean": {
     name: "Grainy Ocean",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768725197/theme-grainy-ocean_znqoth.jpg",
     overlay: "dark",
-    
+    color: "#fffafaff"
   },
   "orange-black": {
     name: "Grainy Orange Black",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768725151/theme-grainy-orange-black_ou0yhc.jpg",
-    overlay: "dark"
+    overlay: "dark",
+    color: "#fff3e0"
   },
   "red": {
     name: "Grainy Red",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768724306/theme-grainy-red_iu7ebt.jpg",
-    overlay: "dark"
+    overlay: "dark",
+    color: "#ffebee"
   },
   "blue": {
     name: "Grainy Blue",
     url: "https://res.cloudinary.com/di1gffhk5/image/upload/v1768724216/theme-grainy-blue_igtzhc.jpg",
     overlay: "dark",
-    
+    color: "#eeeeeeff"
   }
 };
