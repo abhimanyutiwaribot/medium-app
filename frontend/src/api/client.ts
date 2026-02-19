0ba5ab86788056ba4bb93e3c676ffb04f4e65754
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-02-19 19:23:53 +0530

    simplified the client api

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -3,27 +3,22 @@ const API_URL = import.meta.env.VITE_API_URL as string;
 export async function apifetch(
   path: string,
   options: RequestInit = {}
-){
-  const token = localStorage.getItem("token");
-
-  const headers : HeadersInit = {
+) {
+  const headers: HeadersInit = {
     "Content-Type": "application/json",
-    ...(token && {
-      Authorization: `Bearer ${token}`
-    }),
     ...options.headers,
-  }
+  };
 
   const res = await fetch(`${API_URL}${path}`, {
     ...options,
     headers,
+    credentials: "include", // Always send HttpOnly cookies
   });
 
-  if(!res.ok){
-    const err = await res.json().catch(() => ({}));
-    throw err;
+  if (!res.ok) {
+    const err = await res.json().catch(() => ({ error: res.statusText }));
+    throw { status: res.status, ...err };
   }
 
   return res.json();
-
 }
